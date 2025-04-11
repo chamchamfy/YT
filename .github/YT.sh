2be0b7bae6ed3185f6684399b4f32a28c46644cf
Chấm <111698740+chamchamfy@users.noreply.github.com>
2025-04-11 19:06:11 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -66,12 +66,11 @@ echo "  $Vidon"
 if [ "$VERSION" == 'New' ]; then
 VER=$(Xem "https://www.apkmirror.com/apk/google-inc/youtube" | grep 'new in YouTube' | tr -d '[:alpha:]"><=/-' | awk -F"'" '{print $2}' | awk '{$1=$1}{print $1}')
 [ -z "$VER" ] && VER=$Vidon
-Vidon="$VER"
 Kad=Build$Vop
 V=V$Vop2
 elif [ "$VERSION" == 'Auto' ]; then
-VER=$(Xem "https://www.apkmirror.com/apk/google-inc/youtube" | grep 'new in YouTube' | tr -d '[:alpha:]"><=/-' | awk -F"'" '{print $2}' | awk '{$1=$1}{print $1}') 
-[ -z "$VER" ] && VER=$Vidon
+VER=$Vidon
+[ -z "$Vidon" ] && VER=$(Xem "https://www.apkmirror.com/apk/google-inc/youtube" | grep 'new in YouTube' | tr -d '[:alpha:]"><=/-' | awk -F"'" '{print $2}' | awk '{$1=$1}{print $1}')
 Kad=Auto$Vop
 V=U$Vop2
 else
