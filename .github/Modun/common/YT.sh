d6d8f8bb34ccd59a8f1734b1d2f8c8cf5cd32563
Chấm <111698740+chamchamfy@users.noreply.github.com>
2025-05-12 15:26:38 +0700

    Update YT.sh

--- .github/Modun/common/YT.sh
+++ .github/Modun/common/YT.sh
@@ -1,28 +1,31 @@
-# kakathic
+# chamchamfy
 
-checkYT(){
-for Tkvi in $( find /data/app | grep com.google.android.youtube | grep 'base.apk' ); do
-[ "$Tkvi" ] && umount -l "$Tkvi"
-done
-for Vhkdd in $(find /data/app -name *com.google.android.youtube*); do
-[ "$Vhkdd" ] && rm -fr "$Vhkdd"
-done
-}
+linkAPK(){ find /data/app | grep com.google.android.youtube | grep -m1 'base.apk'; }
+
+checkYT(){ 
+Tkvi="$(linkAPK)";
+[ -f "$Tkvi" ] && umount -l "$Tkvi" &>/dev/null;
+[ -d "${Tkvi%/*}" ] && umount -l "${Tkvi%/*}" &>/dev/null;
+[ -d "/data/YouTube/tmp" ] && umount -l /data/YouTube/tmp &>/dev/null;
+rm -fr $MODPATH/YouTube/*; }
 
 installYT(){
-chcon u:object_r:apk_data_file:s0 $1
-[ $(pm install -r $1 | grep -cm1 'Success') == 1 ] && inYT="done" || inYT="failure"
-[ "$inYT" == "failure" ] && pm uninstall com.google.android.youtube >&2
-[ $(pm install -r $1 | grep -cm1 'Success') == 1 ] && inYT="done" || inYT="failure"
-[ "$inYT" == "done" ] || abort "- Error cannot install apk"
-[ "$inYT" == "done" ] || abort "- Error cannot install apk"; }
+chcon u:object_r:apk_data_file:s0 $MODPATH/*.apk
+patpk="$(ls -1 $MODPATH/*.apk | sed '/YouTube.apk/d')"
+[ $(pm install -r $patpk | grep -cm1 'Success') == 1 ] && inYT="done" || inYT="failure"
+[ "$inYT" == "done" ] && pm clear com.google.android.youtube &>/dev/null
+[ "$inYT" == "failure" ] && pm uninstall com.google.android.youtube &>/dev/null
+[ $(pm install -r $patpk | grep -cm1 'Success') == 1 ] && inYT="done" || inYT="failure"
+[ "$inYT" == "done" ] || echo "- Error cannot install apk"; }
 
-linkAPK(){ pm path com.google.android.youtube | grep base | cut -d : -f2; }
-
-cpLIB(){ cp -af $1 ${2%/*}; }
-
-mountYT(){ chcon u:object_r:apk_data_file:s0 "$1"; mount -o bind "$1" "$2"; md1="$(md5sum -b "$1")"; md2="$(md5sum -b "$2")"
-[ "$md1" == "$md2" ] || su -M -c mount -o bind "$1" "$2"; }
+mountYT(){
+if [ -d "${2%/*}" ];then
+cp -acf "${2%/*}"/* "$MODPATH/YouTube";
+mount -t tmpfs YouTube "${2%/*}";
+cp -acf "$MODPATH/YouTube.apk" "$MODPATH/YouTube/base.apk";
+cp -acf "$MODPATH/YouTube"/* "${2%/*}";
+chcon u:object_r:apk_data_file:s0 "$2";
+fi; }
 
 offCH(){
 Sqlite3=$MODPATH/sqlite3
@@ -32,9 +35,8 @@ LDB=$DB/library.db
 LADB=$DB/localappstate.db
 PK=com.google.android.youtube
 cmd appops set --uid $PS GET_USAGE_STATS ignore
-pm disable $PS >&2
+pm disable $PS &>/dev/null
 $Sqlite3 $LDB "UPDATE ownership SET doc_type = '25' WHERE doc_id = '$PK'";
 $Sqlite3 $LADB "UPDATE appstate SET auto_update = '2' WHERE package_name = '$PK'";
 rm -rf /data/data/$PS/cache/*
-pm enable $PS >&2
-}
+pm enable $PS &>/dev/null; }
