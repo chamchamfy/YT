d04dc8a64629920a09eb702ae411af99f88f14f8
Chấm <111698740+chamchamfy@users.noreply.github.com>
2026-03-01 07:01:44 +0700

    up

--- .github/YT.sh
+++ .github/YT.sh
@@ -32,7 +32,7 @@ TaiYT() {
 #urrl="https://www.apkmirror.com"
 #L1="$urrl$(curl -sL -A "$User" "$urrl/apk/$2" | grep -oP 'href="/apk/[^"]+download.php[^"]+"' | head -1)"
 #L2="$urrl$(curl -sL -A "$User" -e "$urrl/apk/$2" "$L1" | grep -oP 'href="/wp-content/themes/APKMirror/download\.php\?[^"]+"' | head -1)"
-L2=$(python3 $HOME/.github/lyt.py "$VER")
+L2=$(curl -sL -A "Mozilla/5.0" "https://youtube.en.uptodown.com/android/versions" | grep -oP "https://youtube.en.uptodown.com/android/download/[0-9]+" | head -n 20 | xargs -I {} curl -sL {} -H "User-Agent: Mozilla/5.0" | grep -oP "https://dw.uptodown.com/dwn/[^\"]+" | head -n 1)
 echo "Link: $L2"
 Taive "$L2" "apk/$1"
 file "apk/$1" | tee "apk/$1.txt";
