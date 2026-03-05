5494c56938044d90e3d0629729b3fb052fd00ca0
Chấm <111698740+chamchamfy@users.noreply.github.com>
2026-03-05 11:02:26 +0700

    UP

--- .github/XYT.sh
+++ .github/XYT.sh
@@ -196,7 +196,7 @@ mv YT.apk $HOME/Tav/YouTube.apk
 cd tmp
 zip -qr $HOME/YT2.apk *
 cd $HOME
-rsign Tav/base.apk YT2.apk $HOME/Up/ZXT-$VER-$ach${amoled2}-rsign.apk
+rsign Tav/base.apk YT2.apk $HOME/Up/XYT-$VER-$ach${amoled2}-rsign.apk
 else
 apksign YT.apk $HOME/Up/XYT-$VER-$ach${amoled2}.apk
 ls Up
